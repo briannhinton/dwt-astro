5413bf6236e8e4b079110c5bae2204659721a88c
Brian <briannhinton@gmail.com>
2023-02-19 20:07:06 -0800

    fix: typing updates

---
+++ src/data/constants.ts
@@ -0,0 +1,14 @@
+export const MENU_LINKS = [
+	{
+		title: "Home",
+		path: "/",
+	},
+	{
+		title: "About",
+		path: "/about",
+	},
+	{
+		title: "Posts",
+		path: "/posts",
+	},
+];
---
+++ src/site.config.ts
@@ -0,0 +1,12 @@
+const siteMeta = {
+	author: "Brian Hinton",
+	title: "Design with Thought",
+	description: "Design with Thought",
+	lang: "en-GB",
+	ogLocale: "en_GB",
+	githubUrl: "https://github.com/briannhinton",
+	themeColorLight: "#fafafa",
+	themeColorDark: "#1d1f21",
+};
+
+export default siteMeta;
